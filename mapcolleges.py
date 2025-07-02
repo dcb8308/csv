4dee9a001a64c8e844f444bd475003676c010ce0
dcb8308 <derrick_bell2@baylor.edu>
2025-07-02 12:52:17 -0500

    Add files via upload

--- mapcolleges.py
+++ mapcolleges.py
@@ -2,43 +2,64 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+# Load data
 with open('schools.geojson') as f:
     geo_data = json.load(f)
 with open('univ.json') as f:
     univ_data = json.load(f)
 
-big12 = [s for s in geo_data['features'] if s['properties'].get('NCAA') == 108]
+big12 = []
+for school in univ_data:
+    if school['NCAA']['NAIA conference number football (IC2020)'] == 108:
+        big12.append(school)
 
-lons, lats, texts, sizes, colors = [], [], [], [], []
+with open('big12_schools.json', 'w') as outfile:
+    json.dump(big12, outfile, indent=4)
+
+lons, lats, texts,  = [], [], []
 
 for school in big12:
-    props = school['properties']
-    name = props['INSTNM']
-    coords = school['geometry']['coordinates']
-    lon, lat = coords
+    for feature in geo_data['features']:
+        if feature['properties']['NAME'] == school['instnm']:
+            lon = school['Longitude location of institution (HD2020)']
+            lat = school['Latitude location of institution (HD2020)']
+            name = school['instnm']
+            enrollment = school['Total enrollment (DRVEF2020)']
+            address = (
+                feature['properties']['street'] + ', ' +
+                feature['properties']['city'] + ', ' +
+                feature['properties']['state'] + ' ' +
+                feature['properties']['zip']
+            )
+            male = school.get('male_enrollment', 'N/A')
+            female = school.get('Percent of total enrollment that are women (DRVEF2020)', 'N/A')
+            text = (
+                f"{name} {address} "
+                f"Total Enrollment: {enrollment} "
+                f"Male: {male} "
+                f"Female: {female}"
+            )
+            lons.append(lon)
+            lats.append(lat)
+            texts.append(text)
+            
+    
 
-    match = next((u for u in univ_data if u['name'].strip() == name.strip()), None)
-    if match:
-        enrollment = match.get('total_enrollment', 0)
-        lons.append(lon)
-        lats.append(lat)
-        texts.append(f"{name}<br>{enrollment:,} students")
-        sizes.append(enrollment / 1500)
-        colors.append(enrollment)
 
-# Plot it
-data = [Scattergeo(
-    lon=lons,
-    lat=lats,
-    text=texts,
-    marker=dict(
-        size=sizes,
-        color=colors,
-        colorscale='Viridis',
-        colorbar=dict(title='Enrollment'),
-        line=dict(width=0.5, color='black')
-    )
-)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': texts,
+    'marker': {
+        'size': [school['Total enrollment (DRVEF2020)'] for school in big12],
+        'color': [school['Total enrollment (DRVEF2020)'] for school in big12],
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Enrollment'},
+        'line': {'width': 0.5, 'color': 'black'}
+    }
+}]
 
-layout = Layout(title='Big 12 Universities (Enrollment)', geo=dict(scope='usa', showland=True))
+layout = Layout(title='Big 12 Universities (Enrollment)')
 offline.plot({'data': data, 'layout': layout}, filename='big12_universities.html')
